a2111d3ca72b6d5b04fa703b1cfc1e375236c279
Vitaliy Rodyna <ukrdao@gmail.com>
2022-09-24 08:26:46 +0300

    const image3 = document.getElementById('image3');

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById('toggle-icon');
 const image1 = document.getElementById('image1');
 const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
