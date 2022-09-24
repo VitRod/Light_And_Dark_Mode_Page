4d9218fc1be446a6647f5b83323ce0c058295116
Vitaliy Rodyna <ukrdao@gmail.com>
2022-09-24 08:24:56 +0300

    const image1 = document.getElementById('image1');

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
