4b140c7733bcda04851fbfb389f04a0b635e2eb9
Vitaliy Rodyna <ukrdao@gmail.com>
2022-09-24 08:27:34 +0300

    const textBox = document.getElementById('text-box');

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ const toggleIcon = document.getElementById('toggle-icon');
 const image1 = document.getElementById('image1');
 const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
