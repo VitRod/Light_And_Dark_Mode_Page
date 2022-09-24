d471620a065dd4f69be9c0ee40dfdd84a0f75602
Vitaliy Rodyna <ukrdao@gmail.com>
2022-09-24 09:30:37 +0300

    nav.style.backgroundColor = dayNight;     textBox.style.backgroundColor = nightDay;     toggleIcon.children[0].textContent = myMode;     toggleIcon.children[1].classList.replace(sun, moon);     // Image Mode through Template Strings     image1.src = `img/undraw_profile_${modeState}.svg`;     image2.src = `img/undraw_developer_activity_${modeState}.svg`;     image3.src = `img/undraw_feedback_${modeState}.svg`; }

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const nightMode = 'Dark Mode';
 const dayMode = 'Light Mode';
 const themeDark = 'dark';
 const themeLight = 'light';
-const dayNight1 = 'rgb(255 255 255 / 50%)'; 
+const dayNight1 = 'rgb(255 255 255 / 50%)';
 const dayNight2 = 'rgb(0 0 0 / 50%)';
 const mySun = 'fa-sun';
 const myMoon = 'fa-moon';
@@ -18,10 +18,16 @@ const dataTheme = 'data-theme';
 
 // Sets Mode Style
 function toggleThemDarkLight(dayNight, nightDay, myMode, sun, moon, modeState) {
-
-
+    // Background color of nav and text box
+    nav.style.backgroundColor = dayNight;
+    textBox.style.backgroundColor = nightDay;
+    toggleIcon.children[0].textContent = myMode;
+    toggleIcon.children[1].classList.replace(sun, moon);
+    // Image Mode through Template Strings
+    image1.src = `img/undraw_profile_${modeState}.svg`;
+    image2.src = `img/undraw_developer_activity_${modeState}.svg`;
+    image3.src = `img/undraw_feedback_${modeState}.svg`;
 }
-
 
 
 
